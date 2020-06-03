c3b801019ba1cfb9b07603328958f2d981103101
Abhishek Saralaya <abhishek13.wonderslate@gmail.com>
2020-06-04 00:51:45 +0530

    Full file write code

--- Write and Read/main.cpp
+++ Write and Read/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void write(string data, string fileName, string extension);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Writing to the file" << endl;
+    cout << "Enter what text you want to enter in the file: ";
+    string data;
+    std::getline(cin, data, '\n');
+    string fileName;
+    string extension;
+    cout << "Enter FileName:";
+    cin >> fileName;
+    cout << "Enter Extension:";
+    cin >> extension;
+    write(data, fileName, extension);
     return 0;
+}
+
+void write(string data, string fileName, string extension){
+    
+    // open the file in write mode.
+    ofstream outfile;
+    string fullName = fileName + "." + extension;
+    
+    outfile.open(fullName);
+    
+    // write the input data into the file.
+    outfile << data << endl;
+    
+    // close the file.
+    outfile.close();
 }
