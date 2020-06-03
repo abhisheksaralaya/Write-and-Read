539f48503b0eb097878416217031a9820ca4ffd4
Abhishek Saralaya <abhishek13.wonderslate@gmail.com>
2020-06-03 20:17:48 +0530

    Initial Commit

---
+++ Write and Read/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Write and Read
+//
+//  Created by Apple on 03/06/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
